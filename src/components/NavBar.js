35afb33c2abebd637f5ab495a12e44a9b4ea0300
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-05-25 22:42:55 -0400

    added functionality to burger menu to toggle nav links show/hide

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
 
+    const toggleNavBarMenu = e => {
+        e.preventDefault();
+        document.querySelector(".navbar-menu").classList.toggle("is-active");
+    }
+
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation" >
             <div className="navbar-brand">
                 <Link className="navbar-item logo" to="/">Cars4<span>Sale</span></Link>
 
-                <a href = "/" onClick = {e => e.preventDefault()} role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a href = "/" onClick = {toggleNavBarMenu} role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
